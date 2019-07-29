291b0fd9bfba3ff1d2c794ce065828c57675e041
Adilson Junior <asc.juninho@hotmail.com>
2019-07-29 15:55:56 -0300

    Retira rota /create

--- src/models/company/controller.js
+++ src/models/company/controller.js
@@ -1,15 +1,15 @@
 const Comp = require('./dao');
 
 module.exports = {
-    async createComp(req, res) {
-        // const company = await Comp.create(req.body);
-        // return res.json(company);
-        const json = req.body;
-        for(let x = 1; x <= 8; x++){
-            await Comp.create(json[x]);
-        }
-        return res.send('Success');
-    },
+    // async createComp(req, res) {
+    //     const company = await Comp.create(req.body);
+    //     return res.json(company);
+    //     const json = req.body;
+    //     for(let x = 1; x <= 8; x++){
+    //         await Comp.create(json[x]);
+    //     }
+    //     return res.send('Success');
+    // },
 
     async getAll(req,res) {
         const data = await Comp.find();
--- src/models/company/routes.js
+++ src/models/company/routes.js
@@ -2,7 +2,7 @@ const controller = require('./controller');
 const express = require('express');
 const routes = express.Router();
 
-routes.post('/create', controller.createComp);
+// routes.post('/create', controller.createComp);
 routes.get('/get', controller.getAll);
 
 module.exports = routes;
