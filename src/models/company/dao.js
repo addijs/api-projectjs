eb6fbd8222e65a4bfcb92c099f47741dea22d519
Adilson Junior <44982030+addijs@users.noreply.github.com>
2020-09-10 16:06:13 -0300

    Atualiza dao.js

--- src/models/company/dao.js
+++ src/models/company/dao.js
@@ -18,4 +18,12 @@ const CompanySchema = new mongoose.Schema({
     }
 });
 
+CompanySchema.methods.toJSON = function() {
+ const obj = this.toObject();
+ delete obj._id;
+ delete obj._v;
+    
+ return obj;
+}
+
 module.exports = mongoose.model('Company', CompanySchema);
